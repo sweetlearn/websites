1eddd075c2ff9c49e1ecdc28ab2272f30f0a7749
Abdulwahed Mansour <info@sweetlearn.com>
2020-03-07 19:53:26 +0200

    bootstrap base

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-
-from . import views
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('result/', views.add, name='result')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# from .models import Question
 
 def home(request):
     template_name = "index.html"
     return render(request, template_name, context=None)
+
+
+def add(request):
+    template_name = "result.html"
+
+    num1 = int(input("Add first Number: "))
+    num2 = int(input("Add scand Number: "))
+
+    total = num1 + num2
+
+    return render(request, template_name, result='total')
